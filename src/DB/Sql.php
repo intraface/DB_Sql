8a33b81e04b7018807c5f6966250a6ddb1483e44
Lars Olesen <lars@intraface.dk>
2010-02-11 09:30:25 +0000

    Fixed some coding standards.
    git-svn-id: http://svn.intraface.dk/intrafacepublic/3Party/DB@1724 2fc64ab5-e821-0410-98e1-ee8f871d7b73

--- src/DB/Sql.php
+++ src/DB/Sql.php
@@ -5,45 +5,45 @@
  */
 require_once 'MDB2.php';
 
-class DB_Sql {
+class DB_Sql
+{
     var $db;
     var $row;
     var $result;
 
-    function DB_Sql($dbhost = '', $dbuser = '', $dbpass = '', $dbname = '') {
+    function DB_Sql($dbhost = '', $dbuser = '', $dbpass = '', $dbname = '')
+    {
+        if (empty($dbhost) OR empty($dbuser) OR empty($dbpass) OR empty($dbname)) {
+            $this->db = MDB2::singleton(DB_DSN);
+        } else {
+            $this->db = MDB2::singleton('mysql://' . $dbuser . ':' . $dbpass . '@' . $dbhost . '/' . $dbname);
+        }
 
-        $this->db = MDB2::singleton(DB_DSN);
         if (PEAR::isError($this->db)) {
             die($this->db->getMessage() . ' ' . $this->db->getUserInfo());
         }
     }
 
-    function query($SQL) {
-        // Eksekvere SQL sætning
-        // $db->Query("SELECT * FROM tabel");
-
-        //echo $SQL . '<br>';
-
+    function query($SQL)
+    {
         $this->result = $this->db->query($SQL);
         if (PEAR::isError($this->result)) {
             die($this->result->getMessage() . ' ' . $this->result->getUserInfo());
         }
-      }
+    }
 
-    function exec($SQL) {
-        // Eksekvere SQL sætning
-        // $db->Query("SELECT * FROM tabel");
-
-        //echo $SQL . '<br>';
-
+    function exec($SQL)
+    {
         $this->result = $this->db->exec($SQL);
         if (PEAR::isError($this->result)) {
             die($this->result->getMessage() . ' ' . $this->result->getUserInfo());
         }
 
         $this->result->free();
-      }
-    function nextRecord() {
+    }
+
+    function nextRecord()
+    {
         // Gennemsøger recordset.
         // Går videre til næste post hver gang den kaldes.
         // Returnere true så længe der er en post
@@ -56,46 +56,51 @@ class DB_Sql {
         return($this->row);
     }
 
-    function affectedRows() {
+    function affectedRows()
+    {
         // returnere antallet af berørte rækker ved INSERT, UPDATE, DELETE
         // print($db->affected_rows());
 
         return($this->db->_affectedRows(NULL));
     }
 
-    function f($name) {
+    function f($name)
+    {
         // Returnere værdien fra feltet med navet som er angivet.
         // Print($db->f("felt"));
         return($this->row[$name]);
     }
 
-    function free() {
+    function free()
+    {
         // Frigør hukommelse til resultatet
         // $db->free();
         $this->result->free();
     }
 
-    function insertedId() {
+    function insertedId()
+    {
         // Returnere det id som lige er blevet indsat
         // $sidste_id = $db->inserted_id();
 
         return($this->db->lastInsertID());
     }
 
-    function numRows() {
+    function numRows()
+    {
         // Returnere antallet af rækker
         // print($db->num_rows());
 
         return($this->result->numRows());
     }
 
-    function escape($value) {
+    function escape($value)
+    {
         return mysql_escape_string($value);
     }
 
-    function quote($value, $type) {
+    function quote($value, $type)
+    {
         return $this->db->quote($value, $type);
-
-
     }
 }
